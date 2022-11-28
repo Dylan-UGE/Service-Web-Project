d4d1a4d5cd098d1af42339bde86af0b0b26b6ff8
Thomas Pourchet <thomas@LAPTOP-4GG95AQE>
2022-11-29 01:17:54 +0200

    little implem in feedback

--- Projet/BikeManager/src/main/java/fr/uge/bike/data/Bike.java
+++ Projet/BikeManager/src/main/java/fr/uge/bike/data/Bike.java
@@ -2,11 +2,16 @@ package fr.uge.bike.data;
 
 import java.io.Serializable;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Bike implements Serializable {
     private int id;
     private String brand;
     private String color;
+    private List<Feedback> feedbacks;
     private int size;
 
     //Field feeback
@@ -18,13 +23,22 @@ public class Bike implements Serializable {
         this.brand = brand;
         this.color = color;
         this.size = size;
-        //this.feedback = new Feedback();
+        this.feedbacks = new ArrayList<Feedback>();
     }
 
     public Bike(int id, String brand, String color, int size) {
         this(brand, color, size);
         this.id = id;
     }
+
+    public double average(){
+        return feedbacks.stream().mapToInt(Feedback::getScore).average().getAsDouble();
+    }
+
+
+    /*public Map<User, String> comments(){
+        return  feedbacks.stream().collect(Collectors.toMap(feedback => ))
+    }*/
 
     public int getId() {
         return id;
--- Projet/BikeManager/src/main/java/fr/uge/bike/data/Feedback.java
+++ Projet/BikeManager/src/main/java/fr/uge/bike/data/Feedback.java
@@ -3,26 +3,27 @@ package fr.uge.bike.data;
 public class Feedback {
     private final User user;
 
-    private final Bike bike;
-
     private final int score;
 
     private final String comment;
 
-    public Feedback(User user, Bike bike, int score, String comment) {
+    public Feedback(User user, int score) {
         this.user = user;
-        this.bike = bike;
+        this.score = score;
+        this.comment = "";
+    }
+
+    public Feedback(User user, int score, String comment) {
+        this.user = user;
         this.score = score;
         this.comment = comment;
     }
+
 
     public User getUser() {
         return user;
     }
 
-    public Bike getBike() {
-        return bike;
-    }
 
     public int getScore() {
         return score;
